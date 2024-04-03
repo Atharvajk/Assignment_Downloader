124ea1f77c27555a1cef65f8383cf8784f4d1948
Atharva <atharva.j.khodke@gmail.com>
2024-04-04 00:18:17 +0530

    added more assignments

--- index.js
+++ index.js
@@ -28,11 +28,72 @@ app.get('/', function(req, res){
       }
     });
 });
+
+
 app.get('/assign1', function(req, res){
     const file = `${__dirname}/assignments/DSBDAL_1.ipynb`;
     res.setHeader('Content-disposition', `attachment; filename=${file}`);
     res.download(file); // Set disposition and send it.
   });
+app.get('/assign2', function(req, res){
+    const file = `${__dirname}/assignments/asign2.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign3', function(req, res){
+    const file = `${__dirname}/assignments/Assign3.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign4', function(req, res){
+    const file = `${__dirname}/assignments/assign4.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign5', function(req, res){
+    const file = `${__dirname}/assignments/asign5.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign6', function(req, res){
+    const file = `${__dirname}/assignments/assign6.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign7', function(req, res){
+    const file = `${__dirname}/assignments/assign7.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign8', function(req, res){
+    const file = `${__dirname}/assignments/assign8-9.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign9', function(req, res){
+    const file = `${__dirname}/assignments/assign8-9.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign10', function(req, res){
+    const file = `${__dirname}/assignments/assign10.ipynb`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign11', function(req, res){
+    const file = `${__dirname}/assignments/WordCount.txt`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+app.get('/assign12', function(req, res){
+    const file = `${__dirname}/assignments/weather.txt`;
+    res.setHeader('Content-disposition', `attachment; filename=${file}`);
+    res.download(file); // Set disposition and send it.
+  });
+
+
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
